a8dd1859a4c243fb134bd072a50537549340ef7a
surviliuks <145654904+5urviliuk5@users.noreply.github.com>
2023-10-12 19:56:35 +0300

    Score and pipe collision

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bird : MonoBehaviour
 {
     public float jumpSpeed;
     public float rotatePower;
     Rigidbody2D rb;
-    
+
+    public TMP_Text BirdScoreText;
+    public int birdScore = 0;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -21,5 +26,22 @@ public class Bird : MonoBehaviour
         }
 
         transform.eulerAngles = new Vector3(0, 0, rb.velocity.y * rotatePower);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Die();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        birdScore++;
+        BirdScoreText.text = birdScore.ToString();
+    }
+
+    void Die()
+    {
+        var sceneName = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(sceneName);
     }
 }
